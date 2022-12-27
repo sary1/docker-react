b1daf3f1ac7298d1880d3492dce294b5b85f2837
Sary <sary.elmelegy@check24.de>
2022-12-27 15:09:02 +0100

    Add updates on a feature branch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <p>Sary Elsayed Abdelkawy Elmelegy</p>
+        <p>This line is added on the feature branch</p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
